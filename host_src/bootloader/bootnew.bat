b621ad4c26dcb4db0467fe9a8cb99008c0c44003
manuel <manuel@457d901e-9eed-11dd-ade5-e746644d0b47>
2004-10-27 23:56:50 +0000

    added batch file to make it easier for the windows folks. This file does the same as bootnew.sh does for Un*x

---
+++ host_src/bootloader/bootnew.bat
@@ -0,0 +1,21 @@
+REM This is a batchfile for DOS/Windows
+REM For the AN851 PIC Bootloader Downloader Program
+REM Copyright (c) 2004 by Manuel Bessler
+REM http://cockpit.varxec.net/electronics/PHCC.html
+
+REM The hexfile has to exist, bark if it doesn't
+IF NOT EXIST %1 GOTO ERROR
+
+an851host.exe --eraseflash --rows 504 --startaddr 0x0200
+an851host.exe --writeflash --hexfile %1
+an851host.exe --reset
+GOTO END
+
+
+:ERROR
+ECHO ERROR: cannot find hexfile %1 !
+ECHO        you have to supply a hexfile on the commandline
+PAUSE
+GOTO END
+
+:END
