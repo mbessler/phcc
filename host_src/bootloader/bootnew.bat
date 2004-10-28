ed383923c06aae0604c6ede6b8692d69f9226204
manuel <manuel@457d901e-9eed-11dd-ade5-e746644d0b47>
2004-10-28 00:12:06 +0000

    added line with echo off quoted %1 in if clause

--- host_src/bootloader/bootnew.bat
+++ host_src/bootloader/bootnew.bat
@@ -1,10 +1,11 @@
+@ECHO OFF
 REM This is a batchfile for DOS/Windows
 REM For the AN851 PIC Bootloader Downloader Program
 REM Copyright (c) 2004 by Manuel Bessler
 REM http://cockpit.varxec.net/electronics/PHCC.html
 
 REM The hexfile has to exist, bark if it doesn't
-IF NOT EXIST %1 GOTO ERROR
+IF NOT EXIST "%1" GOTO ERROR
 
 an851host.exe --eraseflash --rows 504 --startaddr 0x0200
 an851host.exe --writeflash --hexfile %1
